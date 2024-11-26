d31a762085d2fc3d0eaee109309ca6367558e30a
Keith Noguchi <keith@noguchi.us>
2024-11-25 17:17:54 -0800

    ch01: The CPU and the operating system

--- ch01/src/main.rs
+++ ch01/src/main.rs
@@ -1,3 +1,27 @@
+use std::arch::asm;
+
 fn main() {
-    println!("Hello, world!");
+    //let t = 100;
+    //let t_ptr: *const usize = &t;
+    let t_ptr = 999999999999 as *const usize;
+    let x = dereference(t_ptr);
+    println!("{}", x);
+}
+
+#[cfg(target_arch = "x86_64")]
+fn dereference(ptr: *const usize) -> usize {
+    let mut res: usize;
+    unsafe {
+        asm!("mov {0}, [{1}]", out(reg) res, in(reg) ptr)
+    };
+    res
+}
+
+#[cfg(target_arch = "aarch64")]
+fn dereference(ptr: *const usize) -> usize {
+    let mut res: usize;
+    unsafe {
+        asm!("ldr {0}, [{1}]", out(reg) res, in(reg) ptr)
+    };
+    res
 }
