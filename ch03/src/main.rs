5b8578249703887b91b9ea5194a802fe1d33781d
Keith Noguchi <keith@noguchi.us>
2024-11-30 21:59:15 -0800

    fixup! ch03: Understanding OS-backed event queues

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,3 +1,27 @@
+use std::arch::asm;
+
 fn main() {
-    println!("Hello, world!");
+    let message = "Hello world from raw syscall!\n";
+    let msg = message.to_string();
+    syscall(msg)
+}
+
+#[inline(never)]
+fn syscall(msg: String) {
+    let ptr = msg.as_ptr();
+    let n = msg.len();
+
+    unsafe {
+        asm!(
+            "mov x16, 4",
+            "mov x0, 1",
+            "svc 0",
+            in("x1") ptr,
+            in("x2") n,
+            out("x16") _,
+            out("x0") _,
+            lateout("x1") _,
+            lateout("x2") _
+        );
+    }
 }
