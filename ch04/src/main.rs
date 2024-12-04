1a89acd8020efdea88668edfcae52b41df4c51bc
Keith Noguchi <keith@noguchi.us>
2024-12-03 18:46:27 -0800

    fixup! fixup! ch04: Create your own event queue

--- ch04/src/main.rs
+++ ch04/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let bitflag_a: i32 = 1 << 31;
+    let bitflag_b: i32 = 0x1;
+    let bitmask: i32 = bitflag_a|bitflag_b;
+    println!("{bitflag_a:032b}");
+    println!("{bitflag_b:032b}");
+    println!("{bitmask:032b}");
 }
